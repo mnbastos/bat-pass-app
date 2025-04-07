738f9129200d4a788021633a53b44f97b3ed3da0
Marcelo Bastos <marcelonunesbastos@hotmail.com>
2025-04-06 21:25:44 -0300

    separando styles

--- src/screens/Home.tsx
+++ src/screens/Home.tsx
@@ -1,6 +1,7 @@
 import React from 'react'
-import {Text, View, StyleSheet} from 'react-native'
+import {Text, View} from 'react-native'
 import { StatusBar } from 'expo-status-bar';
+import styles from './Style'
 
 export default function Home(){
     return(
@@ -11,11 +12,3 @@ export default function Home(){
     )
 }
 
-const styles = StyleSheet.create({
-    container: {
-      flex: 1,
-      backgroundColor: '#fff',
-      alignItems: 'center',
-      justifyContent: 'center',
-    },
-  });
